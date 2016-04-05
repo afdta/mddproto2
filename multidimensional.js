1dfc418f03c575c4934231c849e35810841b9cca
afdta <afriedhoff@live.com>
2016-04-05 14:56:51 -0400

    In progress... rapid prototyping

--- multidimensional.js
+++ multidimensional.js
@@ -1,13 +1,19 @@
+//to do: account for null values in formatting and display
+
 (function(){
 	var dom = {};
 	var session = {svg:true};
 	var data = {s:null, m:null}; //(s)ingle dimension and (m)ulti-dimensional disadvantage data sets
+	var format = {};
+	format.num = d3.format(",.0");
+	format.share = d3.format(",.1%");
 
 	dom.wrap = d3.select("#multidimensional-disadvantage-wrap");
 	dom.charts = {};
-	dom.charts.single = d3.select("#md-graphics-single");
-	dom.charts.multi = d3.select("#md-graphics-multi");
-	dom.menu = d3.select("#md-menu");
+
+	dom.charts.single = d3.select("#md-graphics-single").style({"padding":"5px","margin":"15px 0px"});
+	dom.charts.multi = d3.select("#md-graphics-multi").style({"padding":"5px"});
+	dom.menu = d3.select("#md-menu")
 
 	if(!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")){
 		dom.wrap.node().innerHTML = '<p style="font-style:italic;text-align:center;margin:30px 0px 30px 0px;">This interactive feature requires a modern browser such as Chrome, Firefox, IE9+, or Safari.</p>';
@@ -27,12 +33,13 @@
 		row.geo = {cbsa: d.CBSA, name:d.Metro};
 		row.race = d.Race;
 		
-		var dis = {};
-		dis.cp = {level: coerceNum(d.ConcPov), share: coerceNum(d.ConcPovSh), title: "Poor locale (living in concentrated poverty)"};
-		dis.le = {level: coerceNum(d.LimitEd), share: coerceNum(d.LimitEdSh), title: "Limited education (no high school diploma)"};
-		dis.li = {level: coerceNum(d.LowInc), share: coerceNum(d.LowIncSh), title: "Low income (150% of federal poverty line)"};
-		dis.hi = {level: coerceNum(d.NoInsure), share: coerceNum(d.NoInsureSh), title: "No health insurance"};
-		dis.nw = {level: coerceNum(d.NonWorking), share: coerceNum(d.NonWorkingSh), title: "Nonworking"};
+		var dis = [
+			{level: coerceNum(d.ConcPov), share: coerceNum(d.ConcPovSh), title: "Poor locale (living in concentrated poverty)"},
+			{level: coerceNum(d.LimitEd), share: coerceNum(d.LimitEdSh), title: "Limited education (no high school diploma)"},
+			{level: coerceNum(d.LowInc), share: coerceNum(d.LowIncSh), title: "Low income (150% of federal poverty line)"},
+			{level: coerceNum(d.NoInsure), share: coerceNum(d.NoInsureSh), title: "No health insurance"},
+			{level: coerceNum(d.NonWorking), share: coerceNum(d.NonWorkingSh), title: "Nonworking"}
+		]
 
 		row.disadvantage = dis;
 		return row;
@@ -44,12 +51,13 @@
 		row.geo = {cbsa: d.CBSA, name:d.Metro};
 		row.race = d.Race;
 
-		var dis = {};
-		dis.licp = {level: coerceNum(d.LI_CP), share: coerceNum(d.LI_CP_SH), title: "Low income and poor locale"};
-		dis.lihi = {level: coerceNum(d.LI_HI), share: coerceNum(d.LI_HI_SH), title: "Low income and no health insurance"};
-		dis.li2p = {level: coerceNum(d.LI_2P), share: coerceNum(d.LI_2P_SH), title: "Low income and two or more other disadvantages"};
-		dis.lile = {level: coerceNum(d.LI_LE), share: coerceNum(d.LI_LE_SH), title: "Low income and limited education"};
-		dis.linw = {level: coerceNum(d.LI_NW), share: coerceNum(d.LI_NW_SH), title: "Low income and nonworking"};
+		var dis = [
+			{level: coerceNum(d.LI_CP), share: coerceNum(d.LI_CP_SH), title: "Low income and poor locale"},
+			{level: coerceNum(d.LI_HI), share: coerceNum(d.LI_HI_SH), title: "Low income and no health insurance"},
+			{level: coerceNum(d.LI_LE), share: coerceNum(d.LI_LE_SH), title: "Low income and limited education"},
+			{level: coerceNum(d.LI_NW), share: coerceNum(d.LI_NW_SH), title: "Low income and nonworking"},
+			{level: coerceNum(d.LI_2P), share: coerceNum(d.LI_2P_SH), title: "Low income and two or more other disadvantages"}
+		]
 
 		row.disadvantage = dis;
 		return row;
@@ -92,31 +100,120 @@
 
 	function setSelect(){
 
-		dom.menu.append("p").text("SELECT A METRO AREA").style({"margin":"3px 15px", "font-size":"11px", "color":"#666666"});
-		dom.select = dom.menu.append("select");
+		dom.select = {};
+		var selwrap1 = dom.menu.append("div").classed("half-width",true);
+		var selwrap2 = dom.menu.append("div").classed("half-width",true);
+
+		selwrap1.append("p").text("SELECT A METRO AREA").style({"margin":"3px 5px", "font-size":"11px", "color":"#666666"});
+		dom.select.metro = selwrap1.append("select").style({"width":"90%"});
+		selwrap2.append("p").text("SELECT A RACE / ETHNICITY").style({"margin":"3px 5px", "font-size":"11px", "color":"#666666"});
+		dom.select.race = selwrap2.append("select").style({"width":"90%"});
 		
-		var options = dom.select.selectAll("option").data(data.metros);
+		var options = dom.select.metro.selectAll("option").data(data.metros);
 		options.enter().append("option");
 		options.exit().remove();
 		options.attr("value",function(d,i){return d.cbsa});
 		options.text(function(d,i){return d.name});
+
+		var opt = dom.select.race.selectAll("option").data([{c:"All", l:"All races/ethnicities"}, 
+															{c:"White", l:"White"}, 
+															{c:"Black", l:"Black"}, 
+															{c:"Hispanic", l:"Hispanic"}]);
+		opt.enter().append("option");
+		opt.exit().remove();
+		opt.attr("value",function(d,i){return d.c});
+		opt.text(function(d,i){return d.l});
 	}
 
-	function drawCharts(cbsa){
-		var single = data.single.map[cbsa];
-		var multi = data.multi.map[cbsa];
-		console.log(single);
-		console.log(multi);
+	function drawCharts(){
+		//need to modify title information based on data
+		dat = getData();
+
+		var maxBar = 200;
+		
+		var g1 = dom.charts.single.selectAll("div").data(dat.single.disadvantage);
+		g1.enter().append("div").classed("one-fifth",true).append("svg").style({"width":"100%", "height":(maxBar+30)+"px", "border-bottom":"1px solid #dddddd"})
+			.append("g").classed("single-bar-chart",true).attr("transform","translate(0,30)");
+		g1.exit().remove();
+
+		var g1Titles = g1.selectAll("p").data(function(d,i){return [d.title]});
+		g1Titles.enter().append("p");
+		g1Titles.exit().remove();
+		g1Titles.text(function(d,i){return d}).style("text-align","center");
+
+		var g1g = g1.select("g.single-bar-chart");
+
+		var g1b = g1g.selectAll("rect").data(function(d,i){return [d]});
+		g1b.enter().append("rect").attr({"width":"50%", "x":"25%", "fill":"red", "stroke":"none"});
+		g1b.exit().remove();
+		g1b.transition()
+			.attr("height", function(d,i){return d.share*maxBar})
+			.attr("y", function(d,i){return maxBar-(d.share*maxBar)});
+
+		var g1t = g1g.selectAll("text.front-text").data(function(d,i){return [d]});
+		g1t.enter().append("text").classed("front-text",true).attr({"x":"50%", "text-anchor":"middle", y:(maxBar-10)+"px"}).style("font-size","28px");
+		g1t.exit().remove();
+		g1t.text(function(d,i){return format.share(d.share)} );
+		g1t.attr("fill",function(d,i){
+			return "red";
+		}).transition().attr("y",function(d,i){
+			return maxBar-(d.share*maxBar)-3;
+		})
+
+
+		var g2 = dom.charts.multi.selectAll("div").data(dat.multi.disadvantage);
+		g2.enter().append("div").classed("one-fifth",true).append("svg").style({"width":"100%", "height":(maxBar+30)+"px", "border-bottom":"1px solid #dddddd"})
+			.append("g").classed("single-bar-chart",true).attr("transform","translate(0,30)");
+		g2.exit().remove();
+
+		var g2Titles = g2.selectAll("p").data(function(d,i){return [d.title]});
+		g2Titles.enter().append("p");
+		g2Titles.exit().remove();
+		g2Titles.text(function(d,i){return d}).style("text-align","center");
+
+		var g2g = g2.select("g.single-bar-chart");
+
+		var g2b = g2g.selectAll("rect").data(function(d,i){return [d]});
+		g2b.enter().append("rect").attr({"width":"50%", "x":"25%", "fill":"red", "stroke":"none"});
+		g2b.exit().remove();
+		g2b.transition()
+			.attr("height", function(d,i){return d.share*maxBar})
+			.attr("y", function(d,i){return maxBar-(d.share*maxBar)});
+
+		var g2t = g2g.selectAll("text.front-text").data(function(d,i){return [d]});
+		g2t.enter().append("text").classed("front-text",true).attr({"x":"50%", "text-anchor":"middle", y:(maxBar-10)+"px"}).style("font-size","28px");
+		g2t.exit().remove();
+		g2t.text(function(d,i){return format.share(d.share)} );
+		g2t.attr("fill",function(d,i){
+			return "red";
+		}).transition().attr("y",function(d,i){
+			return maxBar-(d.share*maxBar)-3;
+		})		
+	}
+
+	function getData(){
+		var metro = dom.select.metro.node().value;
+		var race = dom.select.race.node().value;
+		var single = data.single.map[metro][race][0];
+		var multi = data.multi.map[metro][race][0];
+		return {single:single, multi:multi};
 	}
 
 	function run(){
 		if(data.single && data.multi){
-			//{1} - build select menu
+
+			dom.charts.single.append("p").text("Dimensions of disadvantage").style({"font-size":"18px","padding":"5px 0px", "border-bottom":"1px solid #aaaaaa"});
+			dom.charts.multi.append("p").text("Clustered, or multi-dimensional disadvantages").style({"font-size":"18px","padding":"5px 0px", "border-bottom":"1px solid #aaaaaa"});
+			
+
+			//{1} - build select menus
 			setSelect();
-			//{2} - add select callback
-			dom.select.on("change",function(d,i){
-				drawCharts(this.value);
-			})
+			//{2} - add callbacks
+			dom.select.metro.on("change",drawCharts);
+			dom.select.race.on("change",drawCharts);
+
+			drawCharts();
+
 		}
 	}
 
